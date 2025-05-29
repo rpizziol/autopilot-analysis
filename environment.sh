e8c8599559b8c5409919d15c844c4f8bc7d00764
Roberto Pizziol <roberto.pizziol@gmail.com>
2025-05-29 15:45:07 +0200

    Update variable

--- environment.sh
+++ environment.sh
@@ -18,3 +18,7 @@ DISK_SIZE="10" # GB
 DISK_TYPE="pd-balanced"
 DISK_RESOURCE_POLICY="projects/$PROJECT_ID/regions/$REGION/resourcePolicies/default-schedule-1"
 SCOPES="https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append"
+
+
+
+TARGET_TAG="prometheus-server"
